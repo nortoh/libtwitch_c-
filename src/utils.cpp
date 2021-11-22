6a3c5e49af9ccf85ddc93f4158122d6524cfbd34
Christian Horton <christian@christianhorton.me>
2021-11-22 18:20:04 +0000

    Improved split utility

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,26 +1,37 @@
 #include <iostream>
 #include <string>
+#include <cstring>
 #include <list>
 #include "../includes/utils.h"
 
 using namespace std;
 
 list<string> Utils::split(string input, string delim) {
-    size_t start = 0, end, delim_len = delim.length();
     list<string> result;
-    string token;
-    while((end = input.find(delim, start)) != string::npos) {
-        token = input.substr(start, end - start);
-        start = end + delim_len;
-        result.push_back(token);
+    char input_chars[input.length() + 1];
+    char delim_chars[delim.length() + 1];
+    input.copy(input_chars, sizeof(input_chars), 0);
+    delim.copy(delim_chars, sizeof(delim_chars), 0);
+
+    char* save;
+    char* line;
+
+    line = strtok_r(input_chars, delim.c_str(), &save);
+    while(line != 0) {
+        result.push_back(line);
+        line = strtok_r(0, delim.c_str(), &save);
     }
-    result.push_back(input.substr(start));
     return result;
 }
 
 string Utils::clear_newlines(string input) {
     size_t pos = 0;
     while((pos = input.find("\n", pos)) != string::npos) {
+        input.erase(pos);
+    }
+
+    pos = 0;
+    while((pos = input.find("\r", pos)) != string::npos) {
         input.erase(pos);
     }
     return input;
