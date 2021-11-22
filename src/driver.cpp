614e846d8f1df6280dd41e2620450f51ddda2c04
Christian Horton <christian@christianhorton.me>
2021-11-22 17:22:30 +0000

    Updated driver

--- src/driver.cpp
+++ src/driver.cpp
@@ -1,16 +1,23 @@
 #include <iostream>
 #include "../includes/channel.h"
 #include "../includes/user.h"
+#include "../includes/tsocket.h"
+#include "../includes/config.h"
 
 using namespace std;
 
 int main(int argc, char** argv) {
     cout << "Driver application for twitch bot\n"; 
     
+    Configuration config;
+    
     Channel test_channel ("#test");
     User test_user ("nortoh", test_channel);
     
     cout << "User " << test_user.get_name() << " is in " << test_user.get_channel().name() << "\n";
 
+    cout << "The username is " << Configuration::get("oauth_key") << "\n";
+    
+    start_connection();    
     return 0;
 }
