89065d2d0517e9ddd96bb5fe1c26c41f52c05da8
Christian Horton <christian@christianhorton.me>
2021-11-23 13:40:45 +0000

    Removed test code

--- src/driver.cpp
+++ src/driver.cpp
@@ -10,12 +10,6 @@ int main(int argc, char** argv) {
     cout << "Driver application for twitch bot\n"; 
     
     Configuration config;
-    
-    Channel test_channel ("#test");
-    User test_user ("nortoh", test_channel);
-    
-    cout << "User " << test_user.get_name() << " is in " << test_user.get_channel().name() << "\n";
-
     cout << "The username is " << Configuration::get("oauth_key") << "\n";
     
     start_connection();    
