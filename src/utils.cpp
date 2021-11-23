ad10c206523a8332c28502701fb0faa857b654e9
Christian Horton <christian@christianhorton.me>
2021-11-23 13:39:48 +0000

    Fixed bug with split and remove carriage returns from clear_newlines

--- src/utils.cpp
+++ src/utils.cpp
@@ -8,8 +8,8 @@ using namespace std;
 
 list<string> Utils::split(string input, string delim) {
     list<string> result;
-    char input_chars[input.length()];
-    char delim_chars[delim.length()];
+    char input_chars[input.length() + 1];
+    char delim_chars[delim.length() + 1];
     
     size_t input_len = input.copy(input_chars, sizeof(input_chars), 0);
     size_t delim_len = delim.copy(delim_chars, sizeof(delim_chars), 0);
@@ -31,11 +31,6 @@ list<string> Utils::split(string input, string delim) {
 string Utils::clear_newlines(string input) {
     size_t pos = 0;
     while((pos = input.find("\n", pos)) != string::npos) {
-        input.erase(pos);
-    }
-
-    pos = 0;
-    while((pos = input.find("\r", pos)) != string::npos) {
         input.erase(pos);
     }
     return input;
